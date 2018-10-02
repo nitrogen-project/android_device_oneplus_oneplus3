82d99d89dc4dced8426143533cdf74696b94d2bc
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    op3: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -37,6 +37,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += device/oneplus/oneplus3
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
