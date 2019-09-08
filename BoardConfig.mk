42ca408660667a98346033cc80840b3c789baf59
dianlujitao <dianlujitao@lineageos.org>
2019-09-08 13:39:42 +0800

    op3: Remove custom kernel cross compile prefix
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,6 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8996
 TARGET_KERNEL_CONFIG := nos_oneplus3_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
