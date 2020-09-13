fd7e202a7125e56a9ae3d3f351731acc1c1c8174
xyyx <xyyx@mail.ru>
2020-09-13 06:10:43 +0000

    op3: disable apps

--- device.mk
+++ device.mk
@@ -252,12 +252,12 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml
 
 # Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
+#PRODUCT_PACKAGES += \
+#    OneplusDoze
 
 # Oneplus3Parts
-PRODUCT_PACKAGES += \
-    Oneplus3Parts
+#PRODUCT_PACKAGES += \
+#    Oneplus3Parts
 
 # DRM
 PRODUCT_PACKAGES += \
