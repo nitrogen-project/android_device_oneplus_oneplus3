6c5a87609836cd42c2d14b2de784de4c94ae3ee1
xyyx <xyyx@mail.ru>
2020-09-16 06:20:01 +0000

    op3: use 6gb dalvik props

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 $(call inherit-product, vendor/oneplus/oneplus3/oneplus3-vendor.mk)
 
-$(call inherit-product, vendor/nitrogen/config/phone-xxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, vendor/nitrogen/config/phone-xxhdpi-6144-dalvik-heap.mk)
 $(call inherit-product, vendor/nitrogen/config/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Overlays
